9ff21d359b75a64e05488887add1e374f964d861
kweku afrifa codjoe <kwekuafrifacodjoe@gmail.com>
2022-10-28 02:02:46 -0200

    added freemysqlhosting details

--- php/config.php
+++ php/config.php
@@ -1,5 +1,19 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '', 'vybzuser');
+     //DEVELOPMENT MODE
+    //$host = '127.0.0.1';
+    //$db = 'vybzuser';
+    //$user = 'root';
+    //$pass = '';
+    //$charset = 'utf8mb4';
+    
+    //PRODUCTION MODE
+    $host = 'sql5.freemysqlhosting.net';
+    $db = '	sql5529828';
+    $user = 'sql5529828';
+    $pass = 'W74BmwLEzp';
+    $charset = 'utf8mb4';
+
+    $conn = mysqli_connect($host, $db, $user, $pass, $charset);
     if($conn){
         echo "" .mysqli_connect_error();
     }
