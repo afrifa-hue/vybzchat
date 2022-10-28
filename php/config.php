244385d745a607d74df701bac73b905035846a29
kweku afrifa codjoe <kwekuafrifacodjoe@gmail.com>
2022-10-28 02:33:48 -0200

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,17 +1,17 @@
 <?php
      //DEVELOPMENT MODE
-    //$host = '127.0.0.1';
-    //$db = 'vybzuser';
-    //$user = 'root';
-    //$pass = '';
-    //$charset = 'utf8mb4';
+    $host = '127.0.0.1';
+    $db = 'vybzuser';
+    $user = 'root';
+    $pass = '';
+    $charset = 'utf8mb4';
     
     //PRODUCTION MODE
-    $host = 'sql5.freemysqlhosting.net';
-    $db = '	sql5529828';
-    $user = 'sql5529828';
-    $pass = 'W74BmwLEzp';
-    $charset = 'utf8mb4';
+    //$host = 'sql5.freemysqlhosting.net';
+    //$db = '	sql5529828';
+    //$user = 'sql5529828';
+    //$pass = 'W74BmwLEzp';
+    //$charset = 'utf8mb4';
 
     $conn = mysqli_connect($host, $db, $user, $pass, $charset);
     if($conn){
