3492321a95e2554655f41c7090a5136c0941878a
kweku afrifa codjoe <kwekuafrifacodjoe@gmail.com>
2022-11-02 01:53:16 -0200

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,11 +1,19 @@
+<?php
+    //$serverName = 'sql12.freesqldatabase.com';
+    //$userName = 'sql12534715';
+    //$password = '9tBxWzquPb';
+    //$dbname ='sql12534715';
+    
+    //$conn = mysqli_connect($severName, $userName, $password, $dbname);
+    //if($conn){
+        //echo "" .mysqli_connect_error();
+    //}
+?>
+
 
 <?php
-    $serverName = 'sql12.freesqldatabase.com';
-    $userName = 'sql12534715';
-    $password = '9tBxWzquPb';
-    $dbname ='sql12534715';
     
-    $conn = mysqli_connect($severName, $userName, $password, $dbname);
+    $conn = mysqli_connect('localhost', 'root', '', 'vybzuser');
     if($conn){
         echo "" .mysqli_connect_error();
     }
